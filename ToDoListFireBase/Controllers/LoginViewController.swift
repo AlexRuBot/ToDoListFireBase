8dce664bc2ca9f4fd5f35a8328cf5517f4c4d696
Саша Гужавин <sashaguzavin@gmail.com>
2021-08-01 14:48:41 +0300

    Настроил клавиатуру

--- ToDoListFireBase/Controllers/LoginViewController.swift
+++ ToDoListFireBase/Controllers/LoginViewController.swift
@@ -16,8 +16,18 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(kbDidShow),
+                                               name: UIResponder.keyboardDidShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(kbDidHide),
+                                               name: UIResponder.keyboardDidHideNotification,
+                                               object: nil)
     }
+    
     @IBAction func loginTap(_ sender: UIButton) {
         
     }
@@ -25,6 +35,25 @@ class LoginViewController: UIViewController {
         
     }
     
+    
+    // MARK: - Настройка сдвига экрана при открытии клавиатуры
+    
+    @objc func kbDidShow(notification: Notification) {
+        guard let userInfo = notification.userInfo else {return}
+        let kbFrameSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width,
+                                                          height: self.view.bounds.size.height + kbFrameSize.height)
+        
+        (self.view as! UIScrollView).scrollIndicatorInsets = UIEdgeInsets(top: 0,
+                                                                          left: 0,
+                                                                          bottom: kbFrameSize.height,
+                                                                          right: 0)
+    }
 
+    @objc func kbDidHide() {
+        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.size.width,
+                                                          height: self.view.bounds.size.height)
+    }
 }
 
